c3ba7850b20d1a4fed6af91c0d399041352b1260
Nikorux <nicole.0126@hotmail.es>
2019-12-06 11:06:50 -0400

    Consulta de estudiantes y asignatura

--- OurClient/SOAPClient.py
+++ OurClient/SOAPClient.py
@@ -9,3 +9,12 @@ while True:
     print('◄◄◄◄◄◄◄ Bievenid@ al Portal Estudiantil ►►►►►►►')
     print('\n1) Listar todos los estudiantes\t\n2) Consultar una asignatura\n3) Consultar un profesor')
     opcion = int(input("◄◄◄◄◄◄◄ Ingrese el número de la opción deseada ►►►►►►►: "))
+
+    if opcion == 1:
+        print(client.service.getAllEstudiante())
+
+    if opcion == 2:
+        asignatura = int(input('◄◄◄◄◄◄◄ Digite el nombre de la asignatura ►►►►►►►: '))
+        print(client.service.getAsignatura(asignatura))
+
+    
